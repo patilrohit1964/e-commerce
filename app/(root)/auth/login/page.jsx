95fd40fda7e19f3e50d078a4287ef1168bc3aa47
patilrohit1964 <patilrohit19649@gmail.com>
2025-07-23 23:47:54 +0530

    add form

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,9 +1,21 @@
+import { Card, CardContent } from '@/components/ui/card'
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        
+      <Card className={'w-[450px]'}>
+        <CardContent>
+          <div className='flex justify-center'>
+            <Image src={''} width={'path.width'} height={'path.height'} alt='logo' className='max-w-[150px]'/>
+          </div>
+          <div className='text-center'>
+            <h1></h1>
+            <p></p>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
