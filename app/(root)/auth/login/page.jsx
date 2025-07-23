fb4090e31b615a870935780833a2cbbca7bd1c38
patilrohit1964 <patilrohit19649@gmail.com>
2025-07-23 23:55:47 +0530

    zod add

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,14 +1,22 @@
 import { Card, CardContent } from '@/components/ui/card'
+import { zSchmea } from '@/lib/zodSchema'
 import Image from 'next/image'
 import React from 'react'
-
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 const page = () => {
+  const form = useForm({
+    resolver: zodResolver(zSchmea),
+    defaultValues: {
+      username
+    }
+  })
   return (
     <div>
       <Card className={'w-[450px]'}>
         <CardContent>
           <div className='flex justify-center'>
-            <Image src={''} width={'path.width'} height={'path.height'} alt='logo' className='max-w-[150px]'/>
+            <Image src={''} width={'path.width'} height={'path.height'} alt='logo' className='max-w-[150px]' />
           </div>
           <div className='text-center'>
             <h1></h1>
