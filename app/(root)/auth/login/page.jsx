13b9eb9df3714cb30d1b7fd36b66b9619055d066
patilrohit1964 <patilrohit19649@gmail.com>
2025-07-24 11:45:49 +0530

    add form

--- app/(root)/auth/login/page.jsx
+++ app/(root)/auth/login/page.jsx
@@ -1,16 +1,29 @@
+'use client'
 import { Card, CardContent } from '@/components/ui/card'
 import { zSchmea } from '@/lib/zodSchema'
 import Image from 'next/image'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 const page = () => {
+  const formSchema = zSchmea.pick({ //we can get that method from zoSchema and use here as schema
+    email: true,
+    password: true
+  })
   const form = useForm({
-    resolver: zodResolver(zSchmea),
+    resolver: zodResolver(formSchema),
     defaultValues: {
-      username
+      email: '',
+      password: ''
     }
   })
+
+  const handleLoginSubmit = async (values) => {
+
+  }
   return (
     <div>
       <Card className={'w-[450px]'}>
@@ -23,8 +36,38 @@ const page = () => {
             <p></p>
           </div>
         </CardContent>
-      </Card>
-    </div>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleLoginSubmit)}>
+              <div>
+                <FormField control={form.control} name='email' render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input type={'email'} placeholder="enter your email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}>
+                </FormField>
+              </div>
+              <div>
+                <FormField control={form.control} name='email' render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type={'password'} placeholder="enter your password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}>
+                </FormField>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </Card >
+    </div >
   )
 }
 
