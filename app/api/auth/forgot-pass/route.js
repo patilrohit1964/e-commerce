122bf1489b2bb630ac4862676904b96cb4d59cc7
patilrohit1964 <patilrohit19649@gmail.com>
2025-07-29 19:03:04 +0530

    forgot password pending

--- app/api/auth/forgot-pass/route.js
+++ app/api/auth/forgot-pass/route.js
@@ -1,6 +1,8 @@
 import connectDb from "@/lib/dbConnect";
 import { responce } from "@/lib/helper";
 import { zSchmea } from "@/lib/zodSchema";
+import User from "@/model/user.model";
+import { jwtVerify } from "jose";
 
 export async function POST(req) {
   try {
@@ -8,17 +10,24 @@ export async function POST(req) {
     const validationSchema = zSchmea.pick({
       password: true,
     });
-    const payload = await req.json();
-    const validatedData = validationSchema.safeParse(payload);
-    if (!validatedData.success) {
-      return responce(
-        false,
-        401,
-        "invalid or missing fields",
-        validatedData.error
-      );
-    }
+    const { token } = await req.json();
+    // const validatedData = validationSchema.safeParse(payload);
+    // if (!validatedData.success) {
+    //   return responce(
+    //     false,
+    //     401,
+    //     "invalid or missing fields",
+    //     validatedData.error
+    //   );
+    // }
     const { password } = validatedData.data;
+    const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
+    const decoded = await jwtVerify(token, secret);
+    const user = await User.findByIdAndUpdate(
+      decoded.payload.userId,
+      password,
+      { new: true }
+    );
     return responce(true, 200, "password updated", validatedData.data);
   } catch (error) {
     responce(false, 500, "internal server error", error);
