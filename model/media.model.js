628a42a43376767ccb21eb2755d94758013a3c1e
patilrohit1964 <patilrohit19649@gmail.com>
2025-08-05 10:05:57 +0530

    add cloudinary

---
+++ model/media.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const mediaSchema = new mongoose.Schema(
+  {
+    asset_id: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    public_id: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    path: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    thumbnail_url: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    alt: {
+      type: String,
+      trim: true,
+    },
+    title: {
+      type: String,
+      trim: true,
+    },
+    deletedAt: {
+      type: Date,
+      index: true,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+const MEDIAModel =
+  mongoose.models.Media || mongoose.model("Media", mediaSchema);
+export default MEDIAModel;
