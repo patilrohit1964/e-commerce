1f6b9f411d6f4f52968049a012b97305d02503b4
patilrohit1964 <patilrohit19649@gmail.com>
2025-08-30 11:02:35 +0530

    edit media success

--- app/api/media/update/route.js
+++ app/api/media/update/route.js
@@ -1,3 +1,48 @@
-async function PUT(req) {
-    console.log('req',req);
+import connectDb from "@/lib/dbConnect";
+import { catchError, isAuthenticated, responce } from "@/lib/helper";
+import { zSchmea } from "@/lib/zodSchema";
+import MEDIAModel from "@/model/media.model";
+import { get, isValidObjectId } from "mongoose";
+export async function PUT(req) {
+  try {
+    const auth = await isAuthenticated("admin");
+    if (!auth.isAuth) {
+      return responce(false, 403, "unauthorized");
+    }
+    await connectDb();
+    const payload = await req.json();
+
+    const filter = {
+      deletedAt: null,
+    };
+    if (!isValidObjectId(payload?._id)) {
+      return responce(false, 400, "invalid object id");
+    }
+    const validationEditSchema = zSchmea.pick({
+      _id: true,
+      title: true,
+      alt: true,
+    });
+    const validatedEditData = validationEditSchema.safeParse(payload);
+    if (!validatedEditData.success) {
+      return responce(
+        false,
+        400,
+        "invalid or missing input fields",
+        validatedEditData.error
+      );
+    }
+    const { _id, title, alt } = validatedEditData.data;
+    const getMedia = await MEDIAModel.findById(_id);
+    if (!getMedia) {
+      return responce(false, 400, "media not found");
+    }
+    getMedia.alt = alt;
+    getMedia.title = title;
+    await getMedia.save();
+    return responce(true, 200, "media update successfully");
+  } catch (error) {
+    console.log(error);
+    return catchError(error);
+  }
 }
