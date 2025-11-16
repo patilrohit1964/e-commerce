3105fcbfc55d154c0bd18fd8afc7824dc9c627dd
patilrohit1964 <patilrohit19649@gmail.com>
2025-11-16 18:14:03 +0530

    side bar add for mobile

--- app/(root)/(website)/shop/page.jsx
+++ app/(root)/(website)/shop/page.jsx
@@ -1,9 +1,18 @@
 'use client'
-import Shorting from '../../../../components/application/website/Shorting';
-import Filter from '../../../../components/application/website/Filter';
-import WebsiteBreadCrumb from '../../../../components/application/website/WebsiteBreadCrumb';
-import { WEBSITE_SHOP } from '../../../../routes/websiteRoute';
 import { useState } from 'react';
+import Filter from '../../../../components/application/website/Filter';
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from '../../../../components/ui/sheet';
+import Shorting from '../../../../components/application/website/Shorting';
+import WebsiteBreadCrumb from '../../../../components/application/website/WebsiteBreadCrumb';
+import useWindowSize from '../../../../hooks/useWindowSize';
+import { WEBSITE_SHOP } from '../../../../routes/websiteRoute';
 
 const breadCrumb = {
     title: 'Shop',
@@ -13,17 +22,34 @@ const breadCrumb = {
 }
 const Shop = () => {
     const [limit, setLimit] = useState(0);
+    const [sorting, setSorting] = useState('');
+    const windowSize = useWindowSize();
     return (
         <div>
             <WebsiteBreadCrumb props={breadCrumb} />
             <section className='lg:flex lg:px-32 px-4 my-20'>
-                <div className='w-72 me-4 '>
-                    <div className='sticky top-0 bg-gray-50 p-4 rounded'>
-                        <Filter />
-                    </div>
-                </div>
+                {
+                    windowSize.width > 1024 ?
+                        <div className='w-72 me-4 '>
+                            <div className='sticky top-0 bg-gray-50 p-4 rounded'>
+                                <Filter />
+                            </div>
+                        </div> :
+                        <Sheet>
+                            <SheetTrigger>Open</SheetTrigger>
+                            <SheetContent side='left'>
+                                <SheetHeader>
+                                    <SheetTitle>Are you absolutely sure?</SheetTitle>
+                                    <SheetDescription>
+                                        This action cannot be undone. This will permanently delete your account
+                                        and remove your data from our servers.
+                                    </SheetDescription>
+                                </SheetHeader>
+                            </SheetContent>
+                        </Sheet>
+                }
                 <div className='lg:w-[calc(100%-18rem)]'>
-                    <Shorting limit={limit} setLimit={setLimit} />
+                    <Shorting limit={limit} setLimit={setLimit} sorting={sorting} setSorting={setSorting} />
                 </div>
             </section>
         </div>
--- components/application/website/Shorting.jsx
+++ components/application/website/Shorting.jsx
@@ -1,19 +1,32 @@
 import { Button } from '../../../components/ui/button'
-import React from 'react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../../components/ui/select'
+import { sortings } from '../../../lib/utils'
 
-const Shorting = ({ limit, setLimit, sorting, setSorting }) => {
+const Shorting = ({ limit, setLimit, sorting, setSorting, mobileFilterOpen, setMobileFilterOpen }) => {
     return (
         <div className='flex items-center justify-between flex-wrap gap-2 p-4 bg-gray-50'>
             <ul className='flex items-center gap-4'>
                 <li className='font-semibold'>Show</li>
                 {
-                    [13, 44,35, 41, 43, 4, 23].map((limitNumber, idx) => (
+                    [13, 44, 35, 41, 43,]?.map((limitNumber, idx) => (
                         <li key={idx}>
-                            <Button type='button' className={limitNumber === limit ? 'w-8 h-8 flex justify-center items-center rounded-full border bg-primary text-sm text-white' : 'cursor-pointer '} variant={'ghost'} onClick={()=>setLimit(limitNumber)}>{limitNumber}</Button>
+                            <Button type='button' className={limitNumber === limit ? 'w-8 h-8 flex justify-center items-center rounded-full border bg-primary text-sm text-white' : 'cursor-pointer '} variant={'ghost'} onClick={() => setLimit(limitNumber)}>{limitNumber}</Button>
                         </li>
                     ))
                 }
             </ul>
+            <Select value={sorting} onValueChange={(val) => setSorting(val)}>
+                <SelectTrigger className="md:w-[180px] w-full bg-white">
+                    <SelectValue placeholder="Default Sorting" />
+                </SelectTrigger>
+                <SelectContent>
+                    {
+                        sortings?.map(sort => (
+                            <SelectItem key={sort?.value} value={sort?.value}>{sort?.label}</SelectItem>
+                        ))
+                    }
+                </SelectContent>
+            </Select>
         </div>
     )
 }
