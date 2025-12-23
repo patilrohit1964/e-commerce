5d523f3fec2034383ba731105455d436285c1ea3
patilrohit1964 <patilrohit19649@gmail.com>
2025-12-23 19:00:04 +0530

    add payment gateway now

--- app/(root)/(website)/checkout/page.jsx
+++ app/(root)/(website)/checkout/page.jsx
@@ -9,7 +9,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import ButtonLoading from '../../../../components/application/ButtonLoading'
 import WebsiteBreadCrumb from '../../../../components/application/website/WebsiteBreadCrumb'
 import { Button } from '../../../../components/ui/button'
-import { Form, FormControl, FormField, FormItem, FormMessage } from '../../../../components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../../../../components/ui/form'
 import { Input } from '../../../../components/ui/input'
 import { Label } from '../../../../components/ui/label'
 import { Textarea } from '../../../../components/ui/textarea'
@@ -20,6 +20,7 @@ import { addToCart, clearCart } from '../../../../store/reducers/cartReducer'
 import { showToast } from '../../../../lib/toast'
 import axios from 'axios'
 import { X } from 'lucide-react'
+import z from 'zod'
 const breadCrumb = {
   title: 'Checkout',
   links: [
@@ -30,6 +31,7 @@ const breadCrumb = {
 }
 const Checkout = () => {
   const { cartItems } = useSelector(store => store?.cartStore)
+  const { auth } = useSelector(store => store?.authStore)
   const dispatch = useDispatch()
   const [verifiedCartData, setVerifiedCartData] = useState([]);
   const [isCouponApplied, setIsCouponApplied] = useState(false);
@@ -39,6 +41,7 @@ const Checkout = () => {
   const [totalAmount, setTotalAmount] = useState(0)
   const [total, setTotal] = useState(0)
   const [couponCode, setCouponCode] = useState('')
+  const [orderPlaced, setOrderPlaced] = useState(false);
 
   const { data: getverifiedCartData } = useFetch(`/api/cart-verification`, 'POST', { data: cartItems })
   // get cart verification data logic
@@ -108,57 +111,171 @@ const Checkout = () => {
     setCouponDiscountAmount(0)
     setTotalAmount(total)
   }
+
+  // order place
+  const orderFormSchema = zSchmea?.pick({
+    name: true,
+    email: true,
+    phone: true,
+    country: true,
+    state: true,
+    city: true,
+    pincode: true,
+    landmark: true,
+    ordernote: true,
+  }).extend({
+    userId: z.string().optional()
+  })
+  const orderForm = useForm({
+    resolver: zodResolver(orderFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+      country: '',
+      state: '',
+      city: '',
+      pincode: '',
+      landmark: '',
+      ordernote: '',
+      userId: auth?._id
+    }
+  })
+  const placeOrder = async (values) => {
+    console.log('values',values);
+    setOrderPlaced(true)
+    try {
+
+    }
+    catch (error) {
+      console.log(error)
+    }
+    finally {
+      setOrderPlaced(false)
+    }
+  }
   return (
     <div>
       <WebsiteBreadCrumb props={breadCrumb} />
       {
         cartItems?.length > 0 ?
           <div className='flex lg:flex-nowrap flex-wrap gap-10 my-20 lg:px-32 px-4'>
-            <div className='lg:w-[70%] w-full'>
+            {/* user place order form ui */}
+            <div className='lg:w-[60%] w-full'>
               <div className='mb-3 flex items-center space-x-2 font-semibold'>
                 <LocalShipping />
                 <p>Shipping Address</p>
               </div>
-              <div className='grid md:grid-cols-2 grid-cols-1 gap-4 mb-5'>
-                <div>
-                  <Label htmlFor="first-name" className={'mb-1'}>First Name</Label>
-                  <Input type={'text'} placeholder="First name*" id='first-name' />
-                </div>
-                <div>
-                  <Label htmlFor="email-address" className={'mb-1'}>Email Address</Label>
-                  <Input type={'email'} placeholder='Email Address' id='email-address' />
-                </div>
-                <div>
-                  <Label htmlFor="phone-number" className={'mb-1'}>Phone Number</Label>
-                  <Input type={'tel'} placeholder='Phone Number*' id='phone-number' />
-                </div>
-                <div>
-                  <Label htmlFor="country" className={'mb-1'}>Country</Label>
-                  <Input type={'text'} placeholder='Country' id='country' />
-                </div>
-                <div>
-                  <Label htmlFor="state" className={'mb-1'}>State</Label>
-                  <Input type={'text'} placeholder='State*' id='state' />
-                </div>
-                <div>
-                  <Label htmlFor="city" className={'mb-1'}>City</Label>
-                  <Input type={'text'} placeholder='City*' id='city' />
-                </div>
-                <div>
-                  <Label htmlFor="pin-code" className={'mb-1'}>Pin Code</Label>
-                  <Input type={'text'} placeholder='Pin Code*' id='pin-code' />
-                </div>
-                <div>
-                  <Label htmlFor="landmark" className={'mb-1'}>Landmark</Label>
-                  <Input type={'text'} placeholder='Landmark*' id='landmark' />
-                </div>
-                <div className='col-span'>
-                  <Label htmlFor="order-note" className={'mb-1'}>Order Note</Label>
-                  <Textarea id='order-note' placeholder='Order Note' />
-                </div>
-              </div>
-              <Button className={'w-full bg-black hover:bg-gray-800 text-white cursor-pointer rounded-full'}>Place Order</Button>
+              <Form {...orderForm}>
+                <form onSubmit={orderForm.handleSubmit(placeOrder)}>
+                  <div className='grid md:grid-cols-2 grid-cols-1 gap-4 mb-5'>
+                    <div>
+                      <FormField name='name' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='name'>Name</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='name' {...field} placeholder='Enter your name' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                    <div>
+                      <FormField name='email' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='email'>Email</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='email' {...field} placeholder='Enter your Email' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                    <div>
+                      <FormField name='phone' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='phonenumber'>Phone Number</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='phonenumber' {...field} placeholder='Enter your Phone Number' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                    <div>
+                      <FormField name='country' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='country'>Country</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='country' {...field} placeholder='Enter your Country' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+
+                    <div>
+                      <FormField name='state' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='state'>State</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='state' {...field} placeholder='Enter your State' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                    <div>
+                      <FormField name='city' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='city'>City</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='city' {...field} placeholder='Enter your City' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                    <div>
+                      <FormField name='pincode' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='pincode'>Pin Code</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='pincode' {...field} placeholder='Enter your Pin Code' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                    <div>
+                      <FormField name='landmark' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='landmark'>Landmark</FormLabel>
+                          <FormControl>
+                            <Input type={'text'} id='landmark' {...field} placeholder='Enter your landmark' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                    <div className='col-span-2'>
+                      <FormField name='ordernote' control={orderForm?.control} render={({ field }) => (
+                        <FormItem>
+                          <FormLabel htmlFor='ordernote'>Order Note</FormLabel>
+                          <FormControl>
+                            <Textarea type={'text'} id='ordernote' {...field} placeholder='Enter Order Note' />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )} />
+                    </div>
+                  </div>
+                  <ButtonLoading type='submit' text={'Place Order'} loading={orderPlaced} className={'w-full bg-black hover:bg-gray-800 text-white cursor-pointer rounded-full'} />
+                </form>
+              </Form>
             </div>
+
+            {/* checkout and cart ui */}
             <div className='lg:w-[40%] w-full'>
               <div className='rounded bg-gray-50 p-5 sticky top-5'>
                 <h4 className='text-lg font-semibold mb-5'>Order Summary</h4>
